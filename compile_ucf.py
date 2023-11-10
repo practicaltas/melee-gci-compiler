3cbf7981e72acdd357b2c0bb80b63a23f8ae9f86
practicaltas <practical.tas@gmail.com>
2023-11-10 00:51:14 -0500

    Add release date to compiled gcis

--- compile_ucf.py
+++ compile_ucf.py
@@ -1,11 +1,15 @@
 """compile_ucf.py: compile NTSC and PAL gci files for UCF.
 Usage: python compile_ucf.py"""
 
+import datetime
 import os
-commands = ["python melee_gci_compiler.py -o ucf0.84/ucf0.84ntsc.gci ucf0.84/ucf0.84ntsc.mgc",
-			"python melee_gci_compiler.py -o ucf0.84/ucf0.84ntscfrozenstadium.gci ucf0.84/ucf0.84ntscfrozenstadium.mgc",
-			"python melee_gci_compiler.py --pal -o ucf0.84/ucf0.84pal.gci ucf0.84/ucf0.84pal.mgc",
-			"python melee_gci_compiler.py --pal -o ucf0.84/ucf0.84palfrozenstadium.gci ucf0.84/ucf0.84palfrozenstadium.mgc"]
+
+release_date = datetime.date.today().isoformat()
+
+commands = ["python melee_gci_compiler.py -o ucf0.84/ucf0.84ntsc_" + release_date + ".gci ucf0.84/ucf0.84ntsc.mgc",
+			"python melee_gci_compiler.py -o ucf0.84/ucf0.84ntscfrozenstadium_" + release_date + ".gci ucf0.84/ucf0.84ntscfrozenstadium.mgc",
+			"python melee_gci_compiler.py --pal -o ucf0.84/ucf0.84pal_" + release_date + ".gci ucf0.84/ucf0.84pal.mgc",
+			"python melee_gci_compiler.py --pal -o ucf0.84/ucf0.84palfrozenstadium_" + release_date + ".gci ucf0.84/ucf0.84palfrozenstadium.mgc"]
 
 for command in commands:
 	os.system(command)
